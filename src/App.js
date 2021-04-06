391644f0cfbaa8e23e39a26e5e7c26ed429e3793
jusda-cd-d-huangjiao <491476277@qq.com>
2021-04-06 17:24:40 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [num, setNum] = useState(0)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React123
+        </a>
+        {/* <AppClass /> */}
+      </header>
+    </div>
+  );
+}
+ 
+class AppClass extends React.Component {
+  render() {
+    return <p>KaSong</p>
+  }
+}
+ 
+export default App;
+
